e227ad0bcb00c093e9f89cbb131eac38c0b9ded1
Abdo <aelsharkawi@xion-medical.com>
2024-01-12 12:24:39 +0100

    don't merge: changed the used models and adding BERT

--- backend/Neural_Search/PdfReader.py
+++ backend/Neural_Search/PdfReader.py
@@ -10,20 +10,18 @@ import re
 import logHandler
 logger = logHandler.LogHandler(name="PdfReader").get_logger()
 
-def exclude_special_characters(input_string):
-    """
-    Exclude all chars except numbers and letters from the input string.
+def split_string_to_chunks(input_string, chunk_length, overlap):
+    words = input_string.split()
+    chunks = []
 
-    Parameters:
-    - input_string (str): Input string.
+    while words:
+        chunk = ' '.join(words[:chunk_length])
+        chunks.append(chunk)
+        words = words[chunk_length - overlap:]
 
-    Returns:
-    str: String with special characters removed.
-    """
-    result = re.sub(r'[^\w\s]', '', input_string.replace('\n', ''))
-    return result
+    return chunks
 
-def pdf_to_text(path):
+def pdf_to_text(path, chunk_length, overlap):
   """
   Convert PDF file to plain text.
 
@@ -33,19 +31,18 @@ def pdf_to_text(path):
   Returns:
   dict: {'text':extracted_text, 'paragraphs':paragraphs}.
   """
-  logger.debug(f"Converting {path} to text")
   pdffileobj=open(path,'rb')
   pdfreader=PyPDF2.PdfReader(pdffileobj)
   extracted_text = ""
+  paragraphs = []
 
-  for i, page in enumerate(pdfreader.pages):
-    logger.debug(f"Extracting text from page {i}")
+  for page in pdfreader.pages:
     extracted_text += page.extract_text()
-    paragraphs = [exclude_special_characters(p) for p in extracted_text.split(". \n")]
 
+  paragraphs = split_string_to_chunks(extracted_text, chunk_length, overlap)
   return {'text':extracted_text, 'paragraphs':paragraphs}
 
-def pdf_to_docVec(path, encoder):
+def pdf_to_docVec(path, encoder, chunk_length = 0, remove_stop_words=False, overlap = 30):
   """
   Convert PDF file to DocVec object.
 
@@ -56,12 +53,12 @@ def pdf_to_docVec(path, encoder):
   Returns:
   DocVec: DocVec object containing document vectors and paragraph vectors.
   """
-  logger.debug(f"Converting {path} to DocVec")
-  doc = pdf_to_text(path)
-  vec = encoder.encode(doc['text']).tolist()
-
+  if chunk_length == 0:
+    chunk_length = encoder.max_seq_length
+  doc = pdf_to_text(path, chunk_length, remove_stop_words, overlap)
   paras_vecs = []
   for idp, para in enumerate(doc['paragraphs']):
     paras_vecs.append({"paragraph":para,"vec":encoder.encode(para).tolist()})
+    print(paras_vecs)
 
-  return DocVec(path, vec, paras_vecs)
+  return DocVec(path, doc['text'], paras_vecs)
--- backend/Neural_Search/Qdrant.py
+++ backend/Neural_Search/Qdrant.py
@@ -7,6 +7,7 @@ Creation Date: 11.11.2023
 from qdrant_client import QdrantClient, models
 from qdrant_client.http.models import Filter, FieldCondition, MatchValue
 from sentence_transformers import SentenceTransformer
+from transformers import pipeline
 from fileSystemHandler import FileSystemHandler
 import os
 import config
@@ -21,8 +22,9 @@ class Qdrant:
   - encoder (SentenceTransformer): Sentence embeddings encoder.
   - qdrant_client (QdrantClient): Client for qdrant interactions.
   """
-  def __init__(self, encoder = SentenceTransformer("all-MiniLM-L6-v2"),
-               qdrant_client = QdrantClient(host=config.qdrant_host, port=config.qdrant_port)):
+  def __init__(self, encoder = SentenceTransformer('multi-qa-mpnet-base-dot-v1'),
+               qdrant_client = QdrantClient(host=config.qdrant_host, port=config.qdrant_port),
+               distance=models.Distance.DOT):
     """
     Initialize the Qdrant class.
 
@@ -36,6 +38,8 @@ class Qdrant:
     self.logger = logHandler.LogHandler(name="Qdrant").get_logger()
     self.encoder = encoder
     self.qdrant_client = qdrant_client
+    self.distance = distance
+    self.bert = pipeline(model="distilbert-base-cased-distilled-squad")
 
   def check_user(self, userName):
     """
@@ -49,16 +53,14 @@ class Qdrant:
     """
     try:
       vectors_count = self.qdrant_client.get_collection(userName).vectors_count
-      # print(f"user: {userName} exists, and has {vectors_count} vectors")
-      self.logger.debug(f"user: {userName} exists, and has {vectors_count} vectors")
+      print(f"user: {userName} exists, and has {vectors_count} vectors")
     except:
-      # print(f"user doesn't exist, creating a new collection with the user name: {userName}")
-      self.logger.debug(f"user doesn't exist, creating a new collection with the user name: {userName}")
+      print(f"user doesn't exist, creating a new collection with the user name: {userName}")
       self.qdrant_client.create_collection(
         collection_name=userName,
         vectors_config=models.VectorParams(
             size=self.encoder.get_sentence_embedding_dimension(),
-            distance=models.Distance.COSINE,
+            distance=self.distance,
         ),
       )
       vectors_count = 0
@@ -76,25 +78,17 @@ class Qdrant:
     None
     """
     vectors_count = self.check_user(userName)
-    self.qdrant_client.upload_records(
-    collection_name=userName,
-    records=[
-        models.Record(id=vectors_count, vector=docVec.vec, payload={"isDoc":True, "name":docVec.name})
-    ])
-    vectors_count = vectors_count + 1
 
     for para in docVec.paras_vecs:
-      # print(para['paragraph'])
-      self.logger.debug(f"Vector Count: {vectors_count} Paragraph: {para['paragraph']}")
       self.qdrant_client.upload_records(
       collection_name=userName,
       records=[
-          models.Record(id=vectors_count, vector=para['vec'], payload={"isDoc":False,"name":para['paragraph'], "source_doc":docVec.name})
+          models.Record(id=vectors_count, vector=para['vec'], payload={"doc_name":docVec.name, "passage":para['paragraph']})
       ])
       vectors_count = vectors_count + 1
 
 
-  def get_hits(self, collection_name, search_text, filter):
+  def get_hits(self, collection_name, search_text):
     """
     Get search hits from the Qdrant collection.
 
@@ -106,11 +100,9 @@ class Qdrant:
     Returns:
     list: List of search hits.
     """
-    self.logger.debug(f"Searching for {search_text} in {collection_name}")
     return self.qdrant_client.search(
       collection_name= collection_name,
       query_vector= self.encoder.encode(search_text).tolist(),
-      query_filter= filter,
       limit=8,
     )
 
@@ -124,26 +116,20 @@ class Qdrant:
     Returns:
     str: Name of the vector with the highest score.
     """
-    self.logger.debug(f"Getting scores from search hits")
     max_score_value = -1
     max_score_value_indoc = None
     score_values_indoc = []
     for hit in hits:
         # print(hit.payload, "score:", hit.score)
-        self.logger.debug(f"Payload: {hit.payload} Score: {hit.score}")
-        score_values_indoc.append(hit.payload["name"])
+        score_values_indoc.append(hit.payload)
         if hit.score > max_score_value:
             max_score_value = hit.score
             max_score_value_indoc = hit.payload
-    # print(max_score_value)
-    self.logger.debug(f"Max score: {max_score_value}")
+
     if max_score_value_indoc is not None:
-        # print("The Vector with the highst score:", max_score_value_indoc["name"])
-        self.logger.debug(f"Vector with the highest score: {max_score_value_indoc['name']}")
         return score_values_indoc
     else:
-        # print("No vector")
-        self.logger.debug(f"No vector")
+        print("No vector")
         return "none"
 
   def search(self, collection_name, search_text):
@@ -158,28 +144,16 @@ class Qdrant:
     a list of the most relevant 4 docs. And the most 4 relevant paragraphs by the most relevant doc.
     dict: {"relevant_docs": relevant_doc, "relevant_paragraph": relevant_para}.
     """
-    self.logger.debug(f"Searching for {search_text} in {collection_name}")
-    docs_filter = Filter(must=[FieldCondition(key="isDoc", match=MatchValue(value=True))])
-    docs_hits = self.get_hits(collection_name, search_text, docs_filter)
-    relevant_docs = self.get_scores(docs_hits)
 
-    paras_filter = Filter(must=[FieldCondition(key="source_doc", match=MatchValue(value=relevant_docs[0]))])
-    paras_hits = self.get_hits(collection_name, search_text, paras_filter)
-    relevant_para = self.get_scores(paras_hits)
-    return {"relevant_docs": relevant_docs[:4], "relevant_paragraphs": relevant_para[:4]}
+    docs_hits = self.get_hits(collection_name, search_text)
+    relevant_pasages = self.get_scores(docs_hits)
+    for ans in relevant_pasages:
+      bert_ans = self.bert(search_text, ans['passage'])
+      ans['scentence'] = self.get_full_sentence(ans['passage'], bert_ans['start'], bert_ans['end'])
+
+    return relevant_pasages[:4]
   
   def delete_doc(self, collection_name, doc_name):
-    """
-      Delete a document from the Qdrant collection.
-
-      Parameters:
-      - collection_name (str): Name of the Qdrant collection.
-      - doc_name (str): Name of the document to be deleted.
-
-      Returns:
-      None
-    """
-    self.logger.debug(f"Deleting {doc_name} from {collection_name}")
     self.qdrant_client.delete(
     collection_name=collection_name,
     points_selector=models.Filter(
@@ -197,56 +171,30 @@ class Qdrant:
     )
 
   def revectorize_all(self):
-    self.logger.debug(f"Revectorizing all documents")
     fileHandler = FileSystemHandler(self)
     all_users = [d for d in os.listdir(config.document_directory) if os.path.isdir(os.path.join(config.document_directory, d))]
     for user in all_users:
-        # print(f"deleting {user}")
-        self.logger.debug(f"Deleting {user}")
+        print(f"deleting {user}")
         self.qdrant_client.delete_collection(user)
         files = fileHandler.get_fs_for_user(user)
         for file in files:
-            # print(os.path.join(config.document_directory, user, file['file_name']))
-            self.logger.debug(f"Encoding and uploading {os.path.join(config.document_directory, user, file['file_name'])}")
+            print(os.path.join(config.document_directory, user, file['file_name']))
             file_path = os.path.join(config.document_directory, user, file['file_name'])
             try:
               fileHandler.encode_and_upload(file_path, user)
             except:
-                # print(f"no text recognized in {file_path}")
-                self.logger.debug(f"No text recognized in {file_path}")
+               print(f"no text recognized in {file_path}")
 
-  def rename_doc(self, collection_name, doc_name, new_name):
-      """
-        Rename a document in the Qdrant collection.
-
-        Parameters:
-        - collection_name (str): Name of the Qdrant collection.
-        - doc_name (str): Current name of the document.
-        - new_name (str): New name for the document.
-
-        Returns:
-        bool: True if the renaming is successful, False otherwise.
-      """
-      self.logger.debug(f"Renaming {doc_name} to {new_name} in {collection_name}")
-      self.rename_vec(collection_name, doc_name, new_name, "name")
-      self.rename_vec(collection_name, doc_name, new_name, "source_doc")
-
-  def rename_vec(self, collection_name, doc_name, new_name, key):
-      """
-        looks for all vectors with a specific key and changes the value of this key in the payload
-      """
-      self.qdrant_client.set_payload(
-      collection_name=collection_name,
-      payload={
-          key: new_name,
-      },
-      points=models.Filter(
-        must=[
-            models.FieldCondition(
-                key=key,
-                match=models.MatchValue(value=doc_name),
-            ),
-        ],
-      ),
-      )
-      return True
+  def get_full_sentence(self, input_string, start_index, end_index):
+    
+    # going backwards from start_index, until an end mark is found
+    while start_index > 0 and input_string[start_index - 1] not in ['.', '!', '?']:
+        start_index -= 1
+    
+    # Find the end of the sentence (going forwards from end_index)
+    while end_index < len(input_string) - 1 and input_string[end_index + 1] not in ['.', '!', '?']:
+        end_index += 1
+    
+    full_sentence = input_string[start_index:end_index]
+    
+    return full_sentence
